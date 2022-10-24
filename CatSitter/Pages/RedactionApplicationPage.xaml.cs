7b2ca49e930188a4b4f99b5806120182eb9dd96d
Зиля Шагиахметова <zilyasagiahmetova03@mail.ru>
2022-10-24 20:41:12 +0300

    Сверстала страницу редактирования

---
+++ CatSitter/Pages/RedactionApplicationPage.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CatSitter.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для RedactionApplicationPage.xaml
+    /// </summary>
+    public partial class RedactionApplicationPage : Page
+    {
+        public RedactionApplicationPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnApplication_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCatsitter_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnUserApplication_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnUserRespond_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnAddPhoto_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnDelAmimal_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnAddAnimal_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
